b053f686317335b774b943b1d0945ded49348703
unknown <linus.lindroth.92@gmail.com>
2024-11-27 12:07:18 +0100

    added Add- , Update- and Delete-Item methodes

--- postgres_db_connector/Program.cs
+++ postgres_db_connector/Program.cs
@@ -10,13 +10,35 @@ class Program
         
         // queries.AllItems();
 
-        while (true)
-        {   
-            Console.WriteLine("Skriv in ett id: ");
-            String id = Console.ReadLine();
-            queries.OneItemById(id);
-        }
+        // while (true)
+        // {   
+        //     Console.WriteLine("Skriv in ett id: ");
+        //     String id = Console.ReadLine();
+        //     queries.OneItemById(id);
+        // }
         
         // queries.AllItemWithoutId();
+        
+        // String name = Console.ReadLine();
+        // double price = Convert.ToDouble(Console.ReadLine());
+        // int stock = Convert.ToInt32(Console.ReadLine());
+        // queries.AddItem(name, price, stock);
+
+        // Console.WriteLine("id: ");    
+        // int id = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("name: ");    
+        // String name = Console.ReadLine();
+        // Console.WriteLine("price: ");    
+        // double price = Convert.ToDouble(Console.ReadLine());
+        // Console.WriteLine("stock: ");    
+        // int stock = Convert.ToInt32(Console.ReadLine());
+        //
+        // queries.UpdateItem(id, name, price, stock);
+        
+        Console.WriteLine("id: ");    
+        int id = Convert.ToInt32(Console.ReadLine());
+        
+        queries.DeleteItem(id);
+        
     }
 }
--- postgres_db_connector/Queries.cs
+++ postgres_db_connector/Queries.cs
@@ -50,5 +50,41 @@ public class Queries
                                   $"Stock: {reader.GetInt32(2)}");
             }
     }
+
+    public async void AddItem(String name, double price, int stock)
+    {
+        await using (var cmd = _database.CreateCommand("INSERT INTO ITEMS (name, price, stock) " +
+                                                       "VALUES ($1, $2, $3)"))
+        {
+            cmd.Parameters.AddWithValue(name); // $1 = name 
+            cmd.Parameters.AddWithValue(price); // $2 = price 
+            cmd.Parameters.AddWithValue(stock); // $3 = stock 
+            await cmd.ExecuteNonQueryAsync();
+        }
+    }
+
+    public async void UpdateItem(int id, String name, double price, int stock)
+    {
+        await using (var cmd = _database.CreateCommand("UPDATE items " +
+                                                       "SET name = $1, price = $2, stock = $3 " +
+                                                       " WHERE id = $4"))
+        {
+            cmd.Parameters.AddWithValue(name);
+            cmd.Parameters.AddWithValue(price);
+            cmd.Parameters.AddWithValue(stock);
+            cmd.Parameters.AddWithValue(id);
+            await cmd.ExecuteNonQueryAsync();
+        }
+    }
+
+    public async void DeleteItem(int id)
+    {
+        await using (var cmd = _database.CreateCommand("DELETE FROM items WHERE id = $1"))
+        {
+            cmd.Parameters.AddWithValue(id);
+            int result = await cmd.ExecuteNonQueryAsync();
+            Console.WriteLine(result);
+        }
+    }
     
 }
